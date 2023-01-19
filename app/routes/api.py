7040ef7fac937aecaf3cb983a1a53869354e0c83
Emily Burger <eburger939@gmail.com>
2023-01-19 08:40:19 -0600

    database is seeded, able to register new user and it is saved to the database

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from app.models import User
 from app.db import get_db
 
@@ -8,5 +8,14 @@ bp = Blueprint('api', __name__, url_prefix='/api')
 @bp.route('/users', methods=['POST'])
 def signup():
     data = request.get_json()
-    print(data)
-    return ''
+    db = get_db()
+
+    newUser = User(
+        username= data['username'],
+        email = data['email'],
+        password = data['password']
+    )
+    db.add(newUser)
+    db.commit()
+
+    return jsonify(id = newUser.id)
